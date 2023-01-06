ad9fe62c103594b9ce389166c8d9bdabacbb8bda
LukeMc <lukemccampbell@onebookshelf.com>
2023-01-06 12:16:33 -0500

    python day 7 part 2 (finally)

--- day7_python/main.py
+++ day7_python/main.py
@@ -42,106 +42,21 @@ for l in lines:
         if(d[0] != "dir"):
             handle_file(int(d[0]), d[1])
 
-total = 0
+total_space = 70000000
+used_space = direcs["/"]
+cur_unused = total_space - used_space
+
+unusued_space_needed = 30000000
+need_to_find = unusued_space_needed - cur_unused
+
+print(used_space)
+print(cur_unused)
+print(need_to_find)
+
+lst = []
 for k,v in direcs.items():
-    if(v <= 100000):
-        print(k)
-        total = total + v
+    if( v >= need_to_find ):
+        lst.append((v))
 
-print(total)
-
-
-# direcs = []
-# files = {}
-# pwd = None
-# master_total = 0
-
-# def process_command(line):
-#     global pwd
-
-#     if(line != "$ ls"):
-#         c = line.split(" ")
-#         if(c[2] == "/"):
-#             print("Setting pwd")
-#             pwd = ["/"]
-#             print(pwd)
-#         elif(c[2] == ".."):
-#             pwd.pop()
-#         else:
-#             pwd.append(c[2])
-
-# def add_file(name, size):
-#     p = make_pwd_path()
-
-#     global direcs
-#     global files
-
-#     fn = p + "/" + name
-#     fn = fn.replace("//", "/")
-
-#     global master_total
-#     master_total = master_total + int(size)
-
-#     ft = (p, fn, int(size))
-#     files[fn] = ft
-
-#     if( (p in direcs) == False):
-#         direcs.append(p)
-
-# def make_pwd_path():
-#     global pwd
-#     lpwd = pwd.copy()
-#     print (lpwd)
-#     if(len(lpwd) == 1):
-#         return "/"
-#     else:
-#         lpwd.pop(0)
-#         return '/' + '/'.join(lpwd)
-
-# #######################
-
-# f = open("input.txt", "r")
-# lines = f.read().splitlines()
-
-# for l in lines:
-
-#     if( l.startswith("$") ):
-#         process_command(l)
-#     else:
-#         dat = l.split(" ")
-#         if(dat[0] != "dir"):
-#             add_file(dat[1], dat[0])
-
-# # direcs.sort()
-
-# print("=================================")
-# sizes = {}
-# for d in direcs:
-#     #print("Looking at ",d)
-#     if(sizes.get(d) == None):
-#         sizes[d] = 0
-
-#     for f in files.values():
-#         p = f[0]
-
-#         print("Comparing ~",d,"~ against ~", p, " Result -> ", p.startswith(d))
-
-#         if(p.startswith(d)):
-#             #print(f)
-#             sizes[d] = sizes[d] + f[2]
-
-# print("==============")
-
-# print(sizes)
-# print(master_total)
-
-# total = 0
-# for k,v in sizes.items():
-#     if v <= 100000:
-#         print("-------- ", k, v)
-#         total = total + v
-#     else:
-#         print("Skipping ", k, " ", v)
-
-
-# #print("Res ", total)
+print(lst)
+print(min(lst))
