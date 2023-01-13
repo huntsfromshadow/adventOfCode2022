448682e4fe90058f668a82cf7b3640593854fcbc
LukeMc <lukemccampbell@onebookshelf.com>
2023-01-13 10:23:43 -0500

    day13 part 1

--- day13/main.py
+++ day13/main.py
@@ -1,3 +1,4 @@
+import copy
 import json
 from typing import Optional
 
@@ -52,115 +53,73 @@ def convert(left_v, right_v):
         print(f"- Mixed Types; convert right to {c} and retry comparison")
         return (left_v, c)
 
+def r_comp(left, right) -> Optional[bool]:
 
-def list_compare(left_v, right_v) -> Optional[bool]:
-    idx = 0
-
-    print(f"\t- Compare {left_v} vs {right_v}")    
+    print(f"Rcomp starting {left}, {right}")
 
     while True:
-        print(idx, len(left_v), len(right_v))
-        if idx >= len(left_v) and idx >= len(right_v):
-            # We've walked through both lists return procesing to above
-            return None
-            
-        elif idx >= len(left_v) and idx < len(right_v):
-            # Left has no elements, right does
-            raise Exception("idx >= len(left_v) and idx < len(right_v)")
-            
-        elif idx < len(left_v) and idx >= len(right_v):
-            # Right has no elements, left does
-            raise Exception("idx < len(left_v) and idx >= len(right_v)")
-            
-        else:
-            # both have elements available
-            le = left_v[idx]
-            re = right_v[idx]
-            print(f"\t\tComparing {le} vs {re}")            
-            if le < re:
-                print("1")
-                # If left is smaller then right order
-                print("- Left side is smaller, so inputs are in the right order")
+        print(f"Loop Start left: {left}")
+        print(f"Loop start right: {right}")
+        # check for empty conditions
+        if type(left) == int and type(right) == int:
+            print(f"Compare {left} vs {right}")
+            if left == right:
+                return None
+            elif left < right:
                 return True
-            elif re > le:
-                print("2")
-                # If right is bigger then wrong order
-                print("- Right side is smaller, so inputs are in the right order")
-                return False
             else:
-                print("3")
-                idx = idx + 1    
+                return False
 
+        elif type(left) != list or type(right) != list:
+            # One of the sides is not a list while the other is
+            if type(left) != list:
+                left = [left]
+            else:
+                right = [right]
 
-"""
+            v = r_comp(left, right)
 
+            if v == None:
+                return None
+            else:
+                return v
 
-== Pair 2 ==
-- Compare [[1],[2,3,4]] vs [[1],4]
-  - Compare [1] vs [1]
-    - Compare 1 vs 1
-  - Compare [2,3,4] vs 4
-    - Mixed types; convert right to [4] and retry comparison
-    - Compare [2,3,4] vs [4]
-      - Compare 2 vs 4
-        - Left side is smaller, so inputs are in the right order
+        else:
 
-== Pair 3 ==
-- Compare [9] vs [[8,7,6]]
-  - Compare 9 vs [8,7,6]
-    - Mixed types; convert left to [9] and retry comparison
-    - Compare [9] vs [8,7,6]
-      - Compare 9 vs 8
-        - Right side is smaller, so inputs are not in the right order
+            if len(left) == 0 and len(right) == 0:
+                # Both 0 move on
+                return None
 
-== Pair 4 ==
-- Compare [[4,4],4,4] vs [[4,4],4,4,4]
-  - Compare [4,4] vs [4,4]
-    - Compare 4 vs 4
-    - Compare 4 vs 4
-  - Compare 4 vs 4
-  - Compare 4 vs 4
-  - Left side ran out of items, so inputs are in the right order
+            elif len(left) == 0 and len(right) != 0:
+                return True
 
-== Pair 5 ==
-- Compare [7,7,7,7] vs [7,7,7]
-  - Compare 7 vs 7
-  - Compare 7 vs 7
-  - Compare 7 vs 7
-  - Right side ran out of items, so inputs are not in the right order
+            elif len(left) != 0 and len(right) == 0:
+                return False
 
-== Pair 6 ==
-- Compare [] vs [3]
-  - Left side ran out of items, so inputs are in the right order
+            else:
+                # Both are Lists
+                left_elem = left.pop(0)
+                right_elem = right.pop(0)
 
-== Pair 7 ==
-- Compare [[[]]] vs [[]]
-  - Compare [[]] vs []
-    - Right side ran out of items, so inputs are not in the right order
+                res = r_comp(left_elem, right_elem)
 
-== Pair 8 ==
-- Compare [1,[2,[3,[4,[5,6,7]]]],8,9] vs [1,[2,[3,[4,[5,6,0]]]],8,9]
-  - Compare 1 vs 1
-  - Compare [2,[3,[4,[5,6,7]]]] vs [2,[3,[4,[5,6,0]]]]
-    - Compare 2 vs 2
-    - Compare [3,[4,[5,6,7]]] vs [3,[4,[5,6,0]]]
-      - Compare 3 vs 3
-      - Compare [4,[5,6,7]] vs [4,[5,6,0]]
-        - Compare 4 vs 4
-        - Compare [5,6,7] vs [5,6,0]
-          - Compare 5 vs 5
-          - Compare 6 vs 6
-          - Compare 7 vs 0
-            - Right side is smaller, so inputs are not in the right order
-"""
+                if res == None:
+                    print("Move on")
+                    continue    # Continue on
+                else:
+                    return res
 
 
 
-#######################################
+########################################
 
-f = open("short_input.txt")
+
+
+
+
+
+f = open("input.txt")
 iput = f.read().splitlines()
-
 pairs = []
 wrk = []
 for i in iput:
@@ -172,16 +131,15 @@ for i in iput:
         pairs.append(wrk)
         wrk = []
 
+idx_total = 0
+for idx, p in enumerate(pairs):
+    print(f"== Pair {idx} ==")
+    r = r_comp( copy.deepcopy(p[0]), copy.deepcopy(p[1]))
+    print(f"\tResult: {r}")
 
-idx = 2
-p = pairs[idx]
-print(f"== Pair {idx} ==")
-d = run_compare(p)
+    if r:
+        idx_total = idx_total + idx + 1
 
-print(f"\n\nResult: {d}")
+print(f"\nFinal Result: {idx_total}")
 
-#for idx, p in enumerate(pairs):
-#    print(f"== Pair {idx} ==")
-#    run_compare(p)
-#    quit()
 
